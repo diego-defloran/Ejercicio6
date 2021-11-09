8aa6b03c06c9ed8bf38d75cc7c7e7570f3ba5a27
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-09 17:56:28 -0600

    Update Computadoras.java

--- src/Computadoras.java
+++ src/Computadoras.java
@@ -1,7 +1,7 @@
-/**Pinguino.java 
+/**Computadoras.java 
 *@author Elena Rodríguez 21774 
-*@version 18/10/2021
-*Clase Pinguino que hereda de la clase combatiente  
+*@version 9/11/2021
+*Clase abstracta Computadoras que hereda de Device e implementa interfaces Internet, Videos y Videojuegos 
 */
 import java.util.Date;
 
@@ -12,16 +12,31 @@ abstract class Computadoras extends Device implements Internet, Videos, Videojue
 	}
 	
 	public String NavegarInternet(String url){
+		/** 
+		*Método NavegarInternet
+		*@param url String
+		*@return navegar String con las indicaciones
+		*/
 		String navegar = "Navegando en internet viendo la pagina "+url+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return navegar;
 	}
 
 	public String ReproducirVideos(String video){
+		/** 
+		*Método ReproducirVideos
+		*@param video String 
+		*@return ver String con las indicaciones
+		*/
 		String ver = "Viendo el video "+video+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return ver;
 	}
 	
 	public String EjecutarVideojuegos(String juego){
+		/** 
+		*Método EjecutarVideojuegos
+		*@param juego String 
+		*@return ej String con las indicaciones
+		*/
 		String ej = "Ejecutando el videojuego "+juego+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return ej;
 	}
