b1c5d0019a121970aacbe565ffd06e3646a37641
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-09 18:28:32 -0600

    Update Videos.java

--- src/Videos.java
+++ src/Videos.java
@@ -1,3 +1,9 @@
+/**Videos.java 
+*@author Elena Rodríguez 21774 
+*@version 9/11/2021
+*Interfaz Videos que contiene el méteodo de ReproducirVideos
+*/
+
 public interface Videos{
 	String ReproducirVideos(String video); //Retorna un String con las instrucciones 
 }
