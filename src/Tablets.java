53c9daa86eae56379d83d3a97e1b095132b3bca4
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-09 18:39:44 -0600

    Update Tablets.java

--- src/Tablets.java
+++ src/Tablets.java
@@ -1,8 +1,9 @@
-/**Pinguino.java 
+/**Tablets.java 
 *@author Elena Rodríguez 21774 
-*@version 18/10/2021
-*Clase Pinguino que hereda de la clase combatiente  
+*@version 9/11/2021
+*Clase Tablets que hereda de Device e implementa interfaces Fotografías, Internet, Videos y Portátiles  
 */
+
 import java.util.Date;
 
 class Tablets extends Device implements Fotografias, Internet, Videos, Portatiles {
@@ -12,6 +13,12 @@ class Tablets extends Device implements Fotografias, Internet, Videos, Portatile
 	}
 	
 	public String Probar(int numFuncionalidad, String info){
+		/** 
+		*Método Probar String Selecciona el String con las indicaciones según la opción del usuario
+		*@param numFuncionalidad int numero de la funcionalidad que desea probar el usuario
+		*@para info String con información extra para probar la característica
+		*@return probando String con las instrucciones
+		*/
 		String probando ="";
 		if(numFuncionalidad==1){
 			probando = TomarFotografias(info);
@@ -24,27 +31,50 @@ class Tablets extends Device implements Fotografias, Internet, Videos, Portatile
 	}
 	
 	public String TomarFotografias(String foto){
+		/** 
+		*Método TomarFotografias 
+		*@param foto String 
+		*@return Tomarfoto String con las indicaciones
+		*/
 		String Tomarfoto = "Tomando la fotografia "+foto+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return Tomarfoto;
 	}
 	
 	public String NavegarInternet(String url){
+		/** 
+		*Método NavegarInternet
+		*@param url String
+		*@return navegar String con las indicaciones
+		*/
 		String navegar = "Navegando en internet viendo la pagina "+url+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return navegar;
 	}
 	
 	public String ReproducirVideos(String video){
+		/** 
+		*Método ReproducirVideos
+		*@param video String 
+		*@return ver String con las indicaciones
+		*/
 		String ver = "Viendo el video "+video+" desde mi "+deviceType+" "+manufacturer+" con numero de serie "+serialNumber;
 		return ver;
 	}
 	
 	
 	public String getCaracteristicas(){
+		/** 
+		*Método getCaracteristicas String
+		*@return caracteristicas String con las funcionalidades que puede probar el usuario
+		*/
 		String caracteristicas = "\n1. Tomar fotografias\n"+"2. Navegar en internet\n"+"3. Ver video\n";
 		return caracteristicas;
 	}
 	
 	public int getnumCaracteristicas(){
+		/** 
+		*Método getnumCaracteristicas int
+		*@return num int con el numero de funcionalidades que puede probar el usuario
+		*/
 		int num = 3;
 		return num;
 	}
