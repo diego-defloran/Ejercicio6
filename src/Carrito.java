698f79658dd10556d0689280d715440c68269601
diego-defloran <defl21565@uvg.edu.gt>
2021-11-09 19:14:40 -0600

    Update

--- src/Carrito.java
+++ src/Carrito.java
@@ -2,7 +2,13 @@ import java.util.Random;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.
+
+import com.csvreader.CsvWriter;
+
+import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Carrito {
@@ -15,8 +21,49 @@ public class Carrito {
 	private int numFactura;
 	Random ran1;
 	
-	public void mostarProductos() {
+	public void IntroducirProductos() {
 		
+		
+	}
+	
+	public static void ExportarCSV(List<Device> devices) {
+		String salidaArchivo = "Devices.csv";
+		boolean existe = new File(salidaArchivo).exists();
+		
+		//si existe un archivo lo elimina
+		if(existe) {
+			File archivoDevices = new File(salidaArchivo);
+			archivoDevices.delete();
+		}
+		
+		try {
+			//Crear el archivo
+			CsvWriter salidaCSV = new CsvWriter(new FileWriter(salidaArchivo, true), ',');
+			
+			//Datos para identificar columanas
+			salidaCSV.write("Precio");
+			salidaCSV.write("Marca");
+			salidaCSV.write("Fecha");
+			salidaCSV.write("Serie");
+			salidaCSV.write("MarcadorAR");
+			salidaCSV.write("Tipo");
+			salidaCSV.write("Tienda");
+			
+			salidaCSV.endRecord();//Interumpe la escritura
+			
+			//Recorre la lista y lo inserta
+			
+			for (Device dev : devices) {
+				Device device = null;
+				salidaCSV.write(device.getPrice());
+				
+			}
+
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			
+		}
 	}
 	
 }
