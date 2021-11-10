0f0ceffedb1b1d751d6b1bb8d46ac3b070ec568b
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-09 18:26:04 -0600

    Update Videojuegos.java

--- src/Videojuegos.java
+++ src/Videojuegos.java
@@ -1,4 +1,8 @@
-
+/**Videojuegos.java 
+*@author Elena Rodríguez 21774 
+*@version 9/11/2021
+*Interfaz Videojuegos que contiene el méteodo de EjecutarVideojuegos
+*/
 
 public interface Videojuegos{
 	String EjecutarVideojuegos(String juego); //Retorna un String con las instrucciones 
